df944a0cab4af326b3a006903601b7b50d8e6e17
Konrad Sakowski <sakowski.konrad@gmail.com>
2023-12-03 18:06:49 +0100

    tempo component - default values

--- src/components/tempo/index.vue
+++ src/components/tempo/index.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
   import { useVModel } from '@vueuse/core';
-  import InputText from 'primevue/inputtext';
+  import InputNumber from 'primevue/inputnumber';
   import Slider from 'primevue/slider';
 
-  const props = defineProps<{
-    modelValue: number;
-  }>();
+  const props = withDefaults(
+    defineProps<{
+      modelValue: number;
+      min?: number;
+      max?: number;
+    }>(),
+    {
+      min: 40,
+      max: 70,
+    },
+  );
+
   const emit = defineEmits(['update:modelValue']);
 
   const tempo = useVModel(props, 'modelValue', emit);
 </script>
 
 <template>
-  <div>
-    <InputText v-model.number="tempo" type="number" class="w-full mb-4 text-center" />
-    <Slider v-model="tempo" :min="40" :max="70" class="mx-3" />
+  <div class="tempo">
+    <InputNumber v-model="tempo" :min="min" :max="max" class="w-full mb-4" />
+    <Slider v-model="tempo" :min="min" :max="max" class="mx-3" />
   </div>
 </template>
+
+<style lang="scss">
+.tempo {
+  .p-inputtext {
+    text-align: center;
+  }
+}
+</style>
