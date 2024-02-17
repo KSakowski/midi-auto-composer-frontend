aad980c5ad5c6b444ab5d940b4a0e4725179469b
Konrad Sakowski <sakowski.konrad@gmail.com>
2024-02-17 17:52:47 +0100

    env vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,36 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import VueRouter from 'unplugin-vue-router/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import Layouts from 'vite-plugin-vue-layouts';
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-    // https://github.com/posva/unplugin-vue-router
-    VueRouter({
-      exclude: ['**/components/*.vue'],
-    }),
+  return defineConfig({
+    plugins: [
+      vue(),
 
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
+      // https://github.com/posva/unplugin-vue-router
+      VueRouter({
+        exclude: ['**/components/*.vue'],
+      }),
 
-    // https://github.com/jpkleemans/vite-svg-loader
-    svgLoader(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
+
+      // https://github.com/jpkleemans/vite-svg-loader
+      svgLoader(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-});
+    define: {
+      'process.env': env,
+    },
+  });
+};
